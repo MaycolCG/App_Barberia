3fca7f64fbff412c2efcb2b5ff7d7348f9099ff9
Maycol Carroll <carrollmaycol@gmail.com>
2024-03-04 11:46:23 +0100

    Actualización

--- views/layout.php
+++ views/layout.php
@@ -5,11 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kaliman Barber</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="/build/css/app.css">
 </head>
 <body>
+    <div class="contenedor-app">
+        <div class="imagen"></div>
+        <div class="app">
+            <?php echo $contenido; ?>
+        </div>
+    </div>
 
-    <?php echo $contenido; ?>
+    <?php
+        echo $script ?? '';
+    ?>
             
 </body>
 </html>
